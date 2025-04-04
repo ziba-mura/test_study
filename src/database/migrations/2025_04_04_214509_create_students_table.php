c0d1dc163de7992dc8878941217e87ea6bd7dcfb
ziba-mura <ziba-mura@users.noreply.github.com>
2025-04-05 07:45:28 +0900

    マイグレーションファイル作成

---
+++ src/database/migrations/2025_04_04_214509_create_students_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('hobby')->nullable();
+            $table->integer('grade');
+            $table->boolean('is_deleted')->default(false);
+            $table->timestamps(); // created_at と updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
