4b0ba7b4c4434a6bf16e432f66ef9dc3d7fc3fd7
ziba-mura <ziba-mura@users.noreply.github.com>
2025-04-07 06:32:55 +0900

    AddControllerTest追加

--- src/tests/Feature/Student/AddControllerTest.php
+++ src/tests/Feature/Student/AddControllerTest.php
@@ -14,8 +14,27 @@ class AddControllerTest extends TestCase
     public function test_正しいデータを送信すると登録される()
     {
         $response = $this->post('/student/add', [
+            'name' => str_repeat('あ', 10),
+            'hobby' => str_repeat('あ', 100),
+            'grade' => Grade::FIRST->value,
+        ]);
+
+        $response->assertRedirect('/student/add');
+        $response->assertSessionHas('success', '登録成功！');
+
+        $this->assertDatabaseHas('students', [
+            'name' => str_repeat('あ', 10),
+            'hobby' => str_repeat('あ', 100),
+            'grade' => Grade::FIRST->value,
+            'is_deleted' => 0,
+        ]);
+    }
+
+    public function test_趣味が空でも正しいデータを送信すると登録される()
+    {
+        $response = $this->post('/student/add', [
             'name' => '山田太郎',
-            'hobby' => '読書',
+            'hobby' => null,
             'grade' => Grade::FIRST->value,
         ]);
 
@@ -24,9 +43,58 @@ class AddControllerTest extends TestCase
 
         $this->assertDatabaseHas('students', [
             'name' => '山田太郎',
-            'hobby' => '読書',
+            'hobby' => '',
             'grade' => Grade::FIRST->value,
             'is_deleted' => 0,
         ]);
     }
+
+    public function test_名前が空の場合はバリデーションエラーになる()
+    {
+        $response = $this->from('/student/add')->post('/student/add', [
+            'name' => '',
+            'hobby' => 'テスト趣味',
+            'grade' => Grade::FIRST->value,
+        ]);
+    
+        $response->assertRedirect('/student/add');
+        $response->assertSessionHasErrors('name');
+    }    
+
+    public function test_名前が11文字以上ならバリデーションエラー()
+    {
+        $response = $this->from('/student/add')->post('/student/add', [
+            'name' => str_repeat('あ', 11),
+            'hobby' => '将棋',
+            'grade' => Grade::SECOND->value,
+        ]);
+    
+        $response->assertRedirect('/student/add');
+        $response->assertSessionHasErrors('name');
+    }    
+
+    public function test_趣味が101文字以上ならバリデーションエラー()
+    {
+        $response = $this->from('/student/add')->post('/student/add', [
+            'name' => '山田太郎',
+            'hobby' => str_repeat('あ', 101),
+            'grade' => Grade::SECOND->value,
+        ]);
+    
+        $response->assertRedirect('/student/add');
+        $response->assertSessionHasErrors('hobby');
+    }    
+
+
+    public function test_学年に不正な値を指定するとバリデーションエラー()
+    {
+        $response = $this->from('/student/add')->post('/student/add', [
+            'name' => '佐藤次郎',
+            'hobby' => 'ラジコン',
+            'grade' => 99, // 不正な値
+        ]);
+    
+        $response->assertRedirect('/student/add');
+        $response->assertSessionHasErrors('grade');
+    }    
 }
